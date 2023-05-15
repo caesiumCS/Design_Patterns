16fa4a57b2454d68fe5b3cff0b04134d52829b79
caesiumCS <caesiummm@yandex.ru>
2023-05-16 01:03:40 +0300

    basic classes for factory pattern

--- Factory/Cpp/main.cpp
+++ Factory/Cpp/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+class ITransport {
+public:
+    virtual void move();
+};
+
+class Ship : public ITransport {
+public:
+    void move() override {
+        std::cout << "Ship is moving!" << std::endl;
+    }
+};
+
+class Truck : public ITransport {
+public: 
+    void move() override {
+        std::cout << "Truck is moving!" << std::endl;
+    }
+};
+
 int main(int argc, char** argv)
 {
 	std::cout << "Hello, World!" << std::endl;
